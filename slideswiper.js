4862b800f82c202cbbcbc2ca65c5c94337025582
Roberto Eduardo <robertoemd@gmail.com>
2024-09-30 19:31:02 -0300

    :rocket: atualizacao de portfolio slide swiper substituindo slide circular

---
+++ slideswiper.js
@@ -0,0 +1,26 @@
+var TrandingSlider = new Swiper(".tranding-slider", {
+  effect: "coverflow",
+  grabCursor: false,
+  centeredSlides: true,
+  loop: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 20,
+    stretch: 0,
+    depth: 100,
+    modifier: 2.5,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  autoplay: {
+    delay: 900, // Tempo entre os slides no autoplay
+    disableOnInteraction: false,
+  },
+  speed: 8500, // Tempo da transição entre os slides (2000ms = 2 segundos)
+});
