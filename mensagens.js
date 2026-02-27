52b549dc7d27aa2e8fefedcd966331f119f9421b
Roberto Eduardo <robertoemd@gmail.com>
2026-02-26 23:55:44 -0300

    :hammer: serie de mudanças ux

--- mensagens.js
+++ mensagens.js
@@ -30,36 +30,31 @@ window.addEventListener("scroll", function () {
     iconlk.style.opacity = "0";
     iconwwp.style.opacity = "0";
     iconegithub.style.opacity = "0";
-  } else if (window.scrollY > 600 && window.scrollY < 1200) {
-    teca.classList.add("scrolled");
+  } else if (window.scrollY > 600 && window.scrollY < 1600) {
+     proa.classList.add("scrolled");
+
     inia.classList.remove("scrolled");
+    teca.classList.remove("scrolled");
     expa.classList.remove("scrolled");
-    proa.classList.remove("scrolled");
     cert.classList.remove("scrolled");
     fora.classList.remove("scrolled");
     ctta.classList.remove("scrolled");
-
+    
     iconlk.style.opacity = "1";
     iconwwp.style.opacity = "1";
     iconegithub.style.opacity = "1";
-  } else if (window.scrollY > 1200 && window.scrollY < 1800) {
-    expa.classList.add("scrolled");
+  } else if (window.scrollY > 1600 && window.scrollY < 2600) {
+       expa.classList.add("scrolled");
     inia.classList.remove("scrolled");
     teca.classList.remove("scrolled");
     proa.classList.remove("scrolled");
     cert.classList.remove("scrolled");
     fora.classList.remove("scrolled");
     ctta.classList.remove("scrolled");
-  } else if (window.scrollY > 1800 && window.scrollY < 2400) {
-    proa.classList.add("scrolled");
+    
 
-    inia.classList.remove("scrolled");
-    teca.classList.remove("scrolled");
-    expa.classList.remove("scrolled");
-    cert.classList.remove("scrolled");
-    fora.classList.remove("scrolled");
-    ctta.classList.remove("scrolled");
-  } else if (window.scrollY > 2400 && window.scrollY < 3400) {
+   
+  } else if (window.scrollY > 2600 && window.scrollY < 4200) {
     cert.classList.add("scrolled");
 
     fora.classList.remove("scrolled");
@@ -69,8 +64,17 @@ window.addEventListener("scroll", function () {
     proa.classList.remove("scrolled");
 
     ctta.classList.remove("scrolled");
+ 
+  } else if (window.scrollY > 4200 && window.scrollY < 4800) {
+    teca.classList.add("scrolled");
+    inia.classList.remove("scrolled");
+    expa.classList.remove("scrolled");
+    proa.classList.remove("scrolled");
+    cert.classList.remove("scrolled");
+    fora.classList.remove("scrolled");
+    ctta.classList.remove("scrolled");
     // hoba.classList.remove("scrolled");
-  } else if (window.scrollY > 3400 && window.scrollY < 4000) {
+  } else if (window.scrollY > 4800 && window.scrollY < 5400) {
     fora.classList.add("scrolled");
 
     cert.classList.remove("scrolled");
@@ -79,7 +83,7 @@ window.addEventListener("scroll", function () {
     expa.classList.remove("scrolled");
     proa.classList.remove("scrolled");
     ctta.classList.remove("scrolled");
-  } else if (window.scrollY > 4000 && window.scrollY < 4700) {
+  } else if (window.scrollY > 5400 && window.scrollY < 6000) {
     fora.classList.add("scrolled");
 
     inia.classList.remove("scrolled");
@@ -89,14 +93,14 @@ window.addEventListener("scroll", function () {
     ctta.classList.remove("scrolled");
     cert.classList.remove("scrolled");
     // hoba.classList.remove("scrolled");
-  } else if (window.scrollY > 4700 && window.scrollY < 5500) {
+  } else if (window.scrollY > 6000 && window.scrollY < 6800) {
     fora.classList.remove("scrolled");
     // hoba.classList.add("scrolled");
     ctta.classList.remove("scrolled");
     iconlk.style.opacity = "1";
     iconwwp.style.opacity = "1";
     iconegithub.style.opacity = "1";
-  } else if (window.scrollY >= 5500) {
+  } else if (window.scrollY >= 6800) {
     // hoba.classList.remove("scrolled");
     ctta.classList.add("scrolled");
 
